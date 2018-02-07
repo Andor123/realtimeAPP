7083ac9880c35b065fac0f287e14c6b43b382ee3
Salamon Andor <salamon.andor@gmail.com>
2018-02-07 21:19:18 +0100

    Add new files to realtimeAPP Signed-off-by: Salamon Andor <salamon.andor@gmail.com>

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Real Time APP">
+        <meta name="author" content="Salamon Andor">
+
+        <title>Real Time APP</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+
+    <div id="app">
+        <users></users>
+    </div>
+
+    <template id="users-template">
+        <table class="table">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Creation Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="user in users">
+                <td>@{{ user.id }}</td>
+                <td>@{{ user.name }}</td>
+                <td>@{{ user.email }}</td>
+                <td>@{{ user.created_at }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </template>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+    <script>
+        Vue.component('users', {
+            template: '#users-template',
+            data: function () {
+                return {
+                    users: []
+                }
+            },
+            created: function () {
+                this.getUsers();
+            },
+            methods: {
+                getUsers: function () {
+                    this.users = [
+                        {'id': 1, 'name': 'Andor Salamon', 'email': 'salamon.andor@gmail.com', 'created_at': '2018-02-07 18:49:00'},
+                        {'id': 2, 'name': 'Akos Salamon', 'email': 'salamon.akos@gmail.com', 'created_at': '2018-02-07 18:49:30'}
+                    ]
+                }
+            }
+        });
+        new Vue ({
+            el: '#app'
+        });
+    </script>
+    </body>
+</html>
